bd42c917f4bcaf2092bc5e5ec93fa07cac194fe8
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2023-01-04 14:46:07 -0300

    inicio login

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView } from 'react-native';
+import Login from './components/login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Login/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
