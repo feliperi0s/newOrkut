185a28227129912aa98705366bea3245d2ae6dcf
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2023-01-05 14:54:53 -0300

    ajuste no componente login

--- components/login/login.js
+++ components/login/login.js
@@ -1,15 +1,23 @@
-import { Button, Input } from 'react-native-elements';
-import { View, Text } from 'react-native';
+import { View, Text, Image , Button, TextInput} from 'react-native';
 import styles from './styles'
-import { SafeAreaView } from 'react-native';
 
 export default props => {
     return (
         <View style={styles.container}>
-        <Text>Faça login</Text>
-        <Input style={styles.input}  placeholder="Nome de usuário" />
-        <Input style={styles.input} placeholder="Senha" secureTextEntry />
-        <Button style={styles.button} title="Entrar" />
+        <Image
+        source={require('../../images/Logo.png')}
+        />
+        <Text style={styles.text}>Login</Text>
+        <TextInput style={styles.input} 
+        placeholder="Usuário" 
+        placeholderTextColor="white"
+        />
+        <TextInput style={styles.input} 
+        placeholder="Senha" 
+        secureTextEntry={true}
+        placeholderTextColor="white"
+         />
+        <Button style={styles.button} title="Entrar"/>
       </View>
     )
 }
--- components/login/styles.js
+++ components/login/styles.js
@@ -2,17 +2,36 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'red',
+        backgroundColor: '#EE258F',
         flex:1,
         alignItems:'center',
-        justifyContent:'center'
+        display:'flex',
+        paddingTop:84
     },
     input: {
-        width:200
+        borderWidth:2,
+        borderColor:"white",
+        borderRadius:10,
+        color: 'white',
+        paddingVertical:10,
+        paddingHorizontal:25,
+        width:274,
+        margin: 5
+        
     },    
     button: {
-        width:200
+        width:500,
+        margin:392,
+        backgroundColor:'white',
+        color:'#EE258F'
     },
+    text : {
+        color: 'white',
+        fontWeight: '400',
+        fontSize: 20,
+        paddingBottom:40,
+        paddingTop:40
+    }
 
   });
 
